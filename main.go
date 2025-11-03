778c20d545cce8ad0a1a2159d15b23574bb5573d
DreamingKitten <vecanoi@gmail.com>
2025-11-03 13:00:21 +0200

    Implement basic HTTP server in main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		host, _ := os.Hostname()
+		fmt.Fprintf(w, "Hello from Go on Cloud Run! host=%s\n", host)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("listening on :%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
